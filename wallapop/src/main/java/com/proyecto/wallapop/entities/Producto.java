e463d7c330e8453db0fa54dacf54c7f03bd200bd
Daniel Gómez Cabrerizo <danielgcab@gmail.com>
2020-02-10 17:44:41 +0100

    entidades hechas

--- wallapop/src/main/java/com/proyecto/wallapop/entities/Producto.java
+++ wallapop/src/main/java/com/proyecto/wallapop/entities/Producto.java
@@ -5,13 +5,15 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -26,14 +28,87 @@ public class Producto {
 	@GeneratedValue
 	private int id;
 	
+	@NotEmpty(message="El nombre no puede estar vacío")
+	@Size(min=3, max=25, message="el tamaño tiene que estar entre 3 y 25")
+	@Column(nullable = false)
 	private String nombre;
 	
+	@NotEmpty(message="El campo descripcion no puede estar vacío")
+	@Size(max=200, message="El tamaño máximo es de 200 caracteres")
+	@Column(nullable = false)
 	private String descripcion;
 	
+	@NotEmpty(message="El precio no puede estar vacío")
+	@Column(nullable = false)
 	private BigDecimal precio;
 	
 	 @ManyToOne(fetch = FetchType.LAZY, optional = false)
 	 @JsonIgnore
 	 private Usuario usuario;
+	 
+	 
+
+	public Producto() {
+		super();
+	}
+
+	public Producto(
+			@NotEmpty(message = "El nombre no puede estar vacío") @Size(min = 3, max = 25, message = "el tamaño tiene que estar entre 3 y 25") String nombre,
+			@NotEmpty(message = "El campo descripcion no puede estar vacío") @Size(max = 200, message = "El tamaño máximo es de 200 caracteres") String descripcion,
+			@NotEmpty(message = "El precio no puede estar vacío") BigDecimal precio, Usuario usuario) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.usuario = usuario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	 
+	 
+	 
+	 
 
 }
--- wallapop/src/main/java/com/proyecto/wallapop/entities/Usuario.java
+++ wallapop/src/main/java/com/proyecto/wallapop/entities/Usuario.java
@@ -3,9 +3,13 @@ package com.proyecto.wallapop.entities;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Usuario {
@@ -16,15 +20,102 @@ public class Usuario {
 	@GeneratedValue
 	private int id;
 	
+	@NotEmpty(message="El nombre no puede estar vacío")
+	@Size(min=3, max=15, message="el tamaño tiene que estar entre 3 y 15")
+	@Column(nullable = false)
 	private String nombre;
 	
+	@NotEmpty(message="El apellido no puede estar vacio")
+	@Column(nullable = false)
 	private String apellidos;
 	
+	@NotEmpty(message="El Nick no puede estar vacio")
+	@Column(nullable = false)
 	private String nick;
 	
+	@NotEmpty(message="El email no puede estar vacio")
+	@Email(message="no es una dirección de correo bien formada")
+	@Column(nullable = false)
 	private String email;
 	
+	@Column
 	private String telefono;
+	
+	
+
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(
+			@NotEmpty(message = "El nombre no puede estar vacío") @Size(min = 3, max = 15, message = "el tamaño tiene que estar entre 3 y 15") String nombre,
+			@NotEmpty(message = "El apellido no puede estar vacio") String apellidos,
+			@NotEmpty(message = "El Nick no puede estar vacio") String nick,
+			@NotEmpty(message = "El email no puede estar vacio") @Email(message = "no es una dirección de correo bien formada") String email,
+			String telefono) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.nick = nick;
+		this.email = email;
+		this.telefono = telefono;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
 	
 	
 	
