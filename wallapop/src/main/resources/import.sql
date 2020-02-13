abcda5dd8eef9b304aa0fc72388fa854dbbe3b3f
Daniel Gómez Cabrerizo <danielgcab@gmail.com>
2020-02-13 15:21:04 +0100

    no se

--- wallapop/src/main/resources/import.sql
+++ wallapop/src/main/resources/import.sql
@@ -1,6 +1,7 @@
 insert into usuario (id,nombre,apellidos,email,nick,telefono) values(1,'Pepe','Fernandez','pepito@gmail.es','Don Pepe','666666666');
 insert into usuario (id,nombre,apellidos,email,nick,telefono) values(2,'Jose','Garcia','jose@gmail.es','Josiko','656462862');
 insert into usuario (id,nombre,apellidos,email,nick,telefono) values(3,'Manolo','Romero','manolete@gmail.es','Manolin','697687436');
-insert into producto (id,nombre,descripcion,precio,usuario_id) values(1,'Silla','Una silla de madera',10,1);
-insert into producto (id,nombre,descripcion,precio,usuario_id) values(2,'Mesa','Una mesa de madera',15,1);
-insert into producto (id,nombre,descripcion,precio,usuario_id) values(3,'camiseta','edagejmisdge',12.5,2);
+
+insert into producto (id,nombre,descripcion,imagen,precio,usuario_id) values(1,'Silla','Una silla de madera','assets/cara1.jpg',10,1);
+insert into producto (id,nombre,descripcion,imagen,precio,usuario_id) values(2,'Mesa','Una mesa de madera','assets/cara1.jpg',15,1);
+insert into producto (id,nombre,descripcion,imagen,precio,usuario_id) values(3,'camiseta','edagejmisdge','assets/cara1.jpg',12.5,2);
